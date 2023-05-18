40bd4de2cfb11f7ca80664797680f2980eed69bb
Manjunath Nayak <manjunathnayak2311@gmail.com>
2023-05-18 15:02:06 +0530

    initial commit

---
+++ Backend/Program.cs
@@ -0,0 +1,42 @@
+using MongoDBDotNET6.Models;
+using MongoDBDotNET6.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.Configure<DatabaseSettings>(builder.Configuration.GetSection("DatabaseSettings"));
+builder.Services.AddSingleton<EmployeeService>();
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var MyAllowedOrigins = "_myAllowedOrigins";
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(MyAllowedOrigins,
+                          builder =>
+                          {
+                              builder.WithOrigins("http://localhost:4200")
+                                                  .AllowAnyHeader()
+                                                  .AllowAnyMethod();
+                          });
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.UseCors(MyAllowedOrigins);
+
+app.Run();
